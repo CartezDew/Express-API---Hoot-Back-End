64556bd517379e736191d506dc6030643641cc61
camiegray <camieg4@gmail.com>
2025-04-20 19:22:02 -0700

    Uodate

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import logger from "morgan";
-
+import Router from "./controllers/users.js"
 import authRouter from "./controllers/auth.js";
 import testJwtRouter from "./controllers/test-jwt.js";
 import usersRouter from "./controllers/users.js";
